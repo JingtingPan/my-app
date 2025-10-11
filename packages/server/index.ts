9af6cca2fc5f57fb1cbea7b33650490445f8bc78
Your Name <you@example.com>
2025-10-11 17:51:02 +0800

    Manage deepseek api key

---
+++ packages/server/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import  type { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();// should be the first line, Load environment variables from .env file
+
+
+const app = express();
+const PORT = process.env.PORT || 3000; // Use environment variable or default to 3000
+
+app.get('/',(req: Request,res: Response) =>{
+    res.send("hello world");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
