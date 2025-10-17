a2fa09665a533d2edaceec8f537451b1fbfba36b
Your Name <you@example.com>
2025-10-17 18:26:53 +0800

    run both apps together

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import concurrently from 'concurrently';
+// Run both the server and client concurrently
+concurrently([
+    {   name: 'server',
+        command: 'bun run dev',
+        cwd: 'packages/server',
+        prefixColor: 'cyan',
+    },
+    {   name: 'client',
+        command: 'bun run dev',
+        cwd: 'packages/client',
+        prefixColor: 'green',
+    }
+])
