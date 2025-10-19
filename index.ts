c587f3e52f07cd0c14391fcd17619d82f70d77c1
Your Name <you@example.com>
2025-10-19 17:56:02 +0800

    set up prettier

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
-import concurrently from 'concurrently';
+import concurrently from "concurrently";
 // Run both the server and client concurrently
 concurrently([
-    {   name: 'server',
-        command: 'bun run dev',
-        cwd: 'packages/server',
-        prefixColor: 'cyan',
-    },
-    {   name: 'client',
-        command: 'bun run dev',
-        cwd: 'packages/client',
-        prefixColor: 'green',
-    }
-])
+  {
+    name: "server",
+    command: "bun run dev",
+    cwd: "packages/server",
+    prefixColor: "cyan",
+  },
+  {
+    name: "client",
+    command: "bun run dev",
+    cwd: "packages/client",
+    prefixColor: "green",
+  },
+]);
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,21 +1,20 @@
 import express from 'express';
-import  type { Request, Response } from 'express';
+import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 
-dotenv.config();// should be the first line, Load environment variables from .env file
-
+dotenv.config(); // should be the first line, Load environment variables from .env file
 
 const app = express();
 const PORT = process.env.PORT || 3000; // Use environment variable or default to 3000
 
-app.get('/',(req: Request,res: Response) =>{
-    res.send("hello world");
-})
+app.get('/', (req: Request, res: Response) => {
+   res.send('hello world');
+});
 
-app.get('/api/hello',(req: Request,res: Response) =>{
-    res.send({message: 'Hello from the server!'});
-})
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.send({ message: 'Hello from the server!' });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+   console.log(`Server is running on http://localhost:${PORT}`);
 });
